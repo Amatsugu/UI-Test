76bbdbe34a7407e46fa48e01d7192bf27cdc9b8f
TheDarkVoid <Rohit.Khamraj@gmail.com>
2015-10-09 23:17:55 -0400

    Create Project

---
+++ UI Test/MainWindow.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI_Test
+{
+	public partial class MainWindow : Form
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MainWindow_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void progressBar1_Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
